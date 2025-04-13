e3a4ea7e988dc49f34963ee2c53945843d41982e
Ramya-ui513 <ramyasai1999b@gmail.com>
2025-04-13 17:50:47 +0100

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,7 +15,7 @@ const App = () => {
 
   const fetchProducts = async () => {
     try {
-      const response = await axios.get("http://127.0.0.1:5000/products");
+      const response = await axios.get("http://13.48.58.100:9533/products");
       setProducts(response.data);
     } catch (error) {
       console.error("Error fetching products", error);
